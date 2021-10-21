8b86f7f3b128c0ab44643d5da0f0d8bbdb0a709b
Arjun Gandhi <account@arjugandhi.com>
2021-10-21 14:23:44 -0400

    added a javascript files color

---
+++ web/js/pattern-colors.js
@@ -0,0 +1,21 @@
+/* Copyright (C) 2021 Pattern Labs, Inc - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential */
+window.pattern_colors = {
+    /* lights */
+    "lightest": "#999999",
+    "lighter": "#B7B7B7",
+    "light": "#D5D5D5", 
+    /* darks */
+    "darkish": "#434343",
+    "dark": "#3D3D3D",
+    "darker": "#373737",
+    "darkest":"#2A2A2A",
+    /* colors */
+    "blue": "#3581FA",
+    "red": "#FB6A78",
+    "orange": "#FA8C6B",
+    "yellow": "#FAD17F",
+    "green": "#BEFB89",
+    "purple": "#FB98E9",
+}
